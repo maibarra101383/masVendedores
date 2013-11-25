be36953184c9ffb1d3c6fe2ec40eb6cc53260ba8
Stefania033 <fany8803@hotmail.com>
2013-11-25 17:22:03 -0600

    avance

--- application/controllers/requerimientos.php
+++ application/controllers/requerimientos.php
@@ -47,19 +47,18 @@ class Requerimientos extends CI_Controller{
         $requerimientos  = new Requerimiento();
         $clienteProducto = new Cliente_producto(); 
 
-		$oCliente = $clientes->where('id', $id_cliente)->get();
-        $oRequerimiento =$requerimientos->where('cliente_id',$id_cliente)->get();
-
+		$oCliente       = $clientes->where('id', $id_cliente)->get();
+        $oRequerimiento = $requerimientos->where('cliente_id',$id_cliente)->get();
 
 		if (!$this->input->post()){
     
-			$data['aCliente']   = $oCliente;
+			$data['aCliente']         = $oCliente;
 			$data['aRequerimiento']   = $oRequerimiento;
 
 			$data['error_message'] = "";
 			$data['title'] = "pagina de registro";
 
-		    $data['view']  = "sistema/editar_requerimiento";
+		    $data['view']      = "sistema/editar_requerimiento";
 		    $data['cssFiles']  = array('themes/base/jquery-ui.css','style.css','sistema.css');
             $data['jsFiles']   = array('jquery.js', 
             						   'jquery-ui/ui/jquery-ui.js',
@@ -68,29 +67,37 @@ class Requerimientos extends CI_Controller{
 			$this->load->view('template', $data);
 
 		} else {
-
-	$oCliente->producto->get();
-
-  	foreach ($oCliente->producto->all as $producto){
-
-		$oRequerimiento->notas       = $this->input->post('nota_'.$producto->id);
-	    $oRequerimiento->usuario_id  = $this->input->post('usuario');
-	    $oRequerimiento->cliente_id  = $id_cliente;
-	    $oRequerimiento->descripcion = $this->input->post('des_'.$producto->id);
- 		$oRequerimiento->fecha_m = date("Y-m-d H:i:s");
- 		$oRequerimiento->save();
-
- 		$clienteProducto->where(array('cliente_id'  => $id_cliente,
- 									  'producto_id' => $producto->id))->get();
-
-
- 		$clienteProducto->requerimiento_id = $oRequerimiento->id;
-
-           if ($clienteProducto->save()&& $oRequerimiento->save()){
-				redirect(base_url('clientes/index/1/16/'.$id_vendedor));
+			$requerimientosDelete  = new Requerimiento();
+			
+			if(!$requerimientosDelete->where('cliente_id',$id_cliente)->get()->delete()){
+					echo $requerimientosDelete->error->string;
 			}
 
-		}
+			$oCliente->producto->get();
+
+		  	foreach ($oCliente->producto->all as $producto){
+
+		  		$requerimientosEdit  = new Requerimiento();
+
+		  		$requerimientosEdit->notas       = $this->input->post('nota_'.$producto->id);
+				$requerimientosEdit->usuario_id  = $this->input->post('usuario');
+				$requerimientosEdit->cliente_id  = $id_cliente;
+				$requerimientosEdit->descripcion = $this->input->post('des_'.$producto->id);
+		 		$requerimientosEdit->fecha_m     = date("Y-m-d H:i:s");
+		 		$requerimientosEdit->save();
+
+		 		$clienteProducto->where(array('cliente_id'  => $id_cliente,
+		 									  'producto_id' => $producto->id))->get();
+
+		 		$clienteProducto->requerimiento_id = $requerimientosEdit->id;
+
+		           if ($clienteProducto->save()){
+						
+					}
+
+			}
+
+			redirect(base_url('clientes/index/1/'.$id_vendedor));
 
 	}
   }
--- application/views/sistema/alta_clientes.php
+++ application/views/sistema/alta_clientes.php
@@ -1,223 +1,50 @@
  <h1>Pagina de Registro</h1>
-	 <?php
-	 	/*if (isset($error_menssage)){
-	 		echo '<p>'.$error_menssage.'</p>';
-	 	}*/
-	 	echo '<div class="datagrid" style="width:80; margin:100px auto; border-radius:5px; border:1px solid #58ACFA; pading:20px"><table>';
-	 	echo '<tr>'; 
-	 	echo '<td>';
-	 	echo form_label('DATOS DEL CLIENTE <br><br>');
-	 	echo '</td>';
-	 	echo'</tr>';
-        
+
+<script>
+$(function() {
+$( "#tabs" ).tabs();
+});
+</script>
+
+
+<?php
+echo'<body>';
+echo'<div id="tabs">';
+echo'<ul>';
+echo'<li><a href="#tabs-1">DATOS DEL CLIENTE</a></li>';
+echo'<li><a href="#tabs-2">DATOS GENERALES DEL CONTACTO</a></li>';
+echo'<li><a href="#tabs-3">DATOS DEL PROYECTO O PRODUCTO</a></li>';
+echo'</ul>';
+
+echo'<div id="tabs-1" "style= background-color:#c2ECCFA">';
+echo'<table>';
+echo'<form>';
         echo'<tr>';
-        echo '<td width="20%">';
+        echo '<td width=20% valing="top">';
 	 	echo form_open();
 	 	echo form_label('Nombre de la Empresa : ');
             echo '</td>';
             
-		 	echo '<td  colspan="100%">';
+		 	echo '<td colspan="100%">';
 		 	$data = array(
 		 		'name'  => 'cliente',
 		 		'id'    => 'cliente',
 		 		'value' => set_value('cliente'),
-		 		'style' => 'width:30%'
+		 		'style' => 'width:25%'
 		 	);
 
 		 	echo form_input($data);
 		 	echo form_error('cliente');
 		 	echo '</td>';
 		 	echo'</tr>';
-		 	
-
-            echo '<br>';
-            echo '<br>';
-            echo '<br>';
-            echo '<tr>'; 
-	 	    echo '<td  aling="center" colspan="10%">';
-            echo form_label('  DATOS GENERALES DEL CONTACTO  <br><br>');
-            echo '</td>';
-            echo'</tr>';
-            echo '<br>';
-           
-            echo'<tr>'; 
+		    
+		   
+		    echo'<tr BGCOLOR=>';
             echo '<td width=15% valing="top">';
-            echo form_label('Nombre : ');
-            echo'</td>';
-
-         
-            echo'<td>';  
-		 	$data = array(
-		 		'name'  => 'nombre',
-		 		'id'    => 'nombre',
-		 		'value' => set_value('nombre'),
-		 		'style' => 'width:100%'
-		 	);
-
-		 	echo form_input($data);
-		 	echo form_error('nombre');
-		 	echo'</td>';
-
-     
-           echo'<td colspan="30%">';
-            echo form_label('Apellido Paterno : ');
-            echo'</td>';
-
-            echo'<td colspan="30%">';
-            $data = array(
-		 		'name'  => 'apat',
-		 		'id'    => 'apat',
-		 		'value' => set_value('apat'),
-		 		'style' => 'width:15%'
-		 	);
-
-		 	echo form_input($data);
-		 	echo form_error('apat');
-		 	echo'</td>';
-		 	
-
-		 	echo '<td colspan="30%">';
-		 	echo form_label('Apellido Materno : ');
-		 	echo'</td>';
-
-		 	echo'<td colspan="30%">';
-		 	$data = array(
-		 		'name'  => 'amat',
-		 		'id'    => 'amat',
-		 		'value' => set_value('amat'),
-		 		'style' => 'width:15%'
-		 	);
-
-		 	echo form_input($data);
-		 	echo form_error('amat');
-		 	echo '</td>';
-		 	echo '</tr>';
-            echo'<br>';
-            echo'<br>';
-
-            echo '<tr>';
-            echo '<td colspan=100% >';
-            echo form_label('Email : ');
-            echo'</td>';
-
-            echo'<td>';
-		 	$data = array(
-		 		'name'  => 'email',
-		 		'id'    => 'email',
-		 		'value' => set_value('email'),
-		 		'style' => 'width:20%'
-		 	);
-
-		 	echo form_input($data);
-		 	echo form_error('email');
-		 	echo '</td >';
-		 	echo '</tr>';
-		 	 echo'<br>';
-            
-
-              echo '<br>';
-              echo '<tr>';
-              echo '<td WIDTH=35 colspan="30%">';
-              echo form_label('Lada1 : ');
-		 	$data = array(
-		 		'name'  => 'lada1',
-		 		'id'    => 'lada1',
-		 		'value' => set_value('lada1'),
-		 		'style' => 'width:5%'
-		 	);
-
-		 	echo form_input($data);
-		 	echo form_error('lada1');
-
-              echo form_label('Teléfono 1: ');
-		 	$data = array(
-		 		'name'  => 'telefono1',
-		 		'id'    => 'telefono1',
-		 		'value' => set_value('telefono1'),
-		 		'style' => 'width:10%'
-		 	);
-
-		 	echo form_input($data);
-		 	echo form_error('telefono1');
-
-              echo form_label('Extención 1 : ');
-            
-		 	$data = array(
-		 		'name'  => 'ext1',
-		 		'id'    => 'ext1',
-		 		'value' => set_value('ext1'),
-		 		'style' => 'width:5%'
-		 	);
-
-		 	echo form_input($data);
-		 	echo form_error('ext1');
-		 	echo '</td>';
-		 	echo '</tr>';
-           
-            echo'<br>';
-           echo '<br>';
-
-           echo '<tr>';
-            echo '<td WIDTH=35 colspan="30%">';
-		 	  echo form_label('Lada 2 : ');
-		 	
-		 	$data = array(
-		 		'name'  => 'lada2',
-		 		'id'    => 'lada2',
-		 		'value' => set_value('lada2'),
-		 		'style' => 'width:5%'
-		 	);
-
-		 	echo form_input($data);
-		 	echo form_error('lada2');
-		 	
-		 	  echo form_label('Teléfono 2 : ');
-		 	
-		 	$data = array(
-		 		'name'  => 'telefono2',
-		 		'id'    => 'telefono2',
-		 		'value' => set_value('telefono2'),
-		 		'style' => 'width:10%'
-		 	);
-
-		 	echo form_input($data);
-		 	echo form_error('telefono2');
-
-		 	  echo form_label('Extención 2 : ');
-		 	$data = array(
-		 		'name'  => 'ext2',
-		 		'id'    => 'ext2',
-		 		'value' => set_value('ext2'),
-		 		'style' => 'width:5%'
-		 	);
-
-		 	echo form_input($data);
-		 	echo form_error('ext2');
-		 	echo '</td>';
-		 	echo'</tr>';
-
-             
-             echo'<br>';
-            echo '<br>';
-            echo'</tr>';
-             echo '<td WIDTH=35 colspan="30%">';
-             echo form_label('Dirección : ');
-		 	$data = array(
-		 		'name'  => 'direccion',
-		 		'id'    => 'direccion',
-		 		'value' => set_value('direccion'),
-		 		'style' => 'width:25%'
-		 	);
-
-		 	echo form_input($data);
-		 	echo form_error('direccion');
-		 	echo '</td>';
-		 	echo'</tr>';
-
-            echo '<br>';
-            echo'<tr>';
-            echo '<td WIDTH=35 colspan="30%">';
 		 	echo form_label('Cargo del Cliente : ');
+		 	echo'</td>';
+
+		 	echo '<td  colspan="100%">';
 		 	$data = array(
 		 		'name'  => 'cargo_cliente',
 		 		'id'    => 'cargo_cliente',
@@ -231,10 +58,13 @@
 		 	echo'</tr>';
                    
 
-               echo'<br>';
-               echo'<tr>';
-               echo '<td WIDTH=35 colspan="30%">';
-		 		 echo form_label('Giro de la Empresa : ');
+            
+            echo'<tr>';
+            echo '<td width=15% valing="top">';
+		 	echo form_label('Giro de la Empresa : ');
+		 	echo'</td>';
+
+		    echo '<td  colspan="100%">';
 		 	$data = array(
 		 		'name'  => 'giro_empresa',
 		 		'id'    => 'giro_empresa',
@@ -246,16 +76,225 @@
 		 	echo form_error('giro_empresa');
 		 	echo '</td>';
 		 	echo'</tr>';
+echo'</table>';		 	
+echo'</div>';
 
-            echo '<br>';
+
+
+echo'<div id="tabs-2">';
+echo'<table>';
+ echo'<tr>'; 
+            echo '<td width=15% valing="top">';
+            echo form_label('Nombre : ');
+            echo'</td>';
+
+         
+            echo'<td colpan="15%">';  
+		 	$data = array(
+		 		'name'  => 'nombre',
+		 		'id'    => 'nombre',
+		 		'value' => set_value('nombre'),
+		 		'style' => 'width:50%'
+		 	);
+
+		 	echo form_input($data);
+		 	echo form_error('nombre');
+		 	echo'</td>';
+
+           
+           echo'<td width=15% valing="top">';
+            echo form_label('Apellido Paterno : ');
+            echo'</td>';
+
+            echo'<td colpan="15%">';  
+		 	$data = array(
+		 		'name'  => 'apat',
+		 		'id'    => 'apat',
+		 		'value' => set_value('apat'),
+		 		'style' => 'width:50%'
+		 	);
+
+		 	echo form_input($data);
+		 	echo form_error('apat');
+		 	echo'</td>';
+		 	
+
+		 	echo '<td width="15%">';
+		 	echo form_label('Apellido Materno : ');
+		 	echo'</td>';
+
+		 	echo'<td colspan="15%">';
+		 	$data = array(
+		 		'name'  => 'amat',
+		 		'id'    => 'amat',
+		 		'value' => set_value('amat'),
+		 		'style' => 'width:50%'
+		 	);
+
+		 	echo form_input($data);
+		 	echo form_error('amat');
+		 	echo '</td>';
+		 	echo '</tr>';
+            
+
+            echo '<tr>';
+            echo '<td width=15%>';
+            echo form_label('Email : ');
+            echo'</td>';
+
+            echo'<td colspan ="20%">';
+		 	$data = array(
+		 		'name'  => 'email',
+		 		'id'    => 'email',
+		 		'value' => set_value('email'),
+		 		'style' => 'width:35%'
+		 	);
+
+		 	echo form_input($data);
+		 	echo form_error('email');
+		 	echo '</td>';
+		 	echo '</tr>';
+		 	
+           
+              echo '<tr>';
+              echo '<td width=15% valing="top">';
+              echo form_label('Lada1 : ');
+              echo'</td>';
+
+              echo'<td width=15% valing="top">';
+		 	$data = array(
+		 		'name'  => 'lada1',
+		 		'id'    => 'lada1',
+		 		'value' => set_value('lada1'),
+		 		'style' => 'width:30%'
+		 	);
+
+		 	echo form_input($data);
+		 	echo form_error('lada1');
+		 	echo '</td>';
+
+             echo '<td width=15% valing="top">';
+              echo form_label('Teléfono 1: ');
+              echo '</td>';
+
+              echo '<td width=15% valing="top">';
+		 	$data = array(
+		 		'name'  => 'telefono1',
+		 		'id'    => 'telefono1',
+		 		'value' => set_value('telefono1'),
+		 		'style' => 'width:50%'
+		 	);
+
+		 	echo form_input($data);
+		 	echo form_error('telefono1');
+		 	echo '</td>';
+
+              echo '<td width=15% valing="top">';
+              echo form_label('Extención 1 : ');
+              echo '</td>';
+
+              echo '<td width=15% valing="top">';
+              $data = array(
+		 		'name'  => 'ext1',
+		 		'id'    => 'ext1',
+		 		'value' => set_value('ext1'),
+		 		'style' => 'width:30%'
+		 	);
+
+		 	echo form_input($data);
+		 	echo form_error('ext1');
+		 	echo '</td>';
+		 	echo '</tr>';
+           
+          
+
+           echo '<tr>';
+            echo '<td width=15% valing="top">';
+		 	  echo form_label('Lada 2 : ');
+		 	  echo '</td>';
+		 	
+		 	echo '<td width=15% valing="top">';
+		 	$data = array(
+		 		'name'  => 'lada2',
+		 		'id'    => 'lada2',
+		 		'value' => set_value('lada2'),
+		 		'style' => 'width:30%'
+		 	);
+
+		 	echo form_input($data);
+		 	echo form_error('lada2');
+		 	echo '</td>';
+
+		 	echo '<td width=15% valing="top">';
+		 	  echo form_label('Teléfono 2 : ');
+		 	  echo '</td>';
+
+		 	echo '<td width=15% valing="top">';
+		 	$data = array(
+		 		'name'  => 'telefono2',
+		 		'id'    => 'telefono2',
+		 		'value' => set_value('telefono2'),
+		 		'style' => 'width:50%'
+		 	);
+
+		 	echo form_input($data);
+		 	echo form_error('telefono2');
+		 	echo '</td>';
+
+              echo '<td width=15% valing="top">';
+		 	  echo form_label('Extención 2 : ');
+		 	  echo '</td>';
+
+		 	echo '<td width=15% valing="top">';
+		 	$data = array(
+		 		'name'  => 'ext2',
+		 		'id'    => 'ext2',
+		 		'value' => set_value('ext2'),
+		 		'style' => 'width:30%'
+		 	);
+
+		 	echo form_input($data);
+		 	echo form_error('ext2');
+		 	echo '</td>';
+		 	echo'</tr>';
+
+             
             echo'<tr>';
-            echo '<td WIDTH=35 colspan="30%">';
+            echo '<td width=15% valing="top">';
+             echo form_label('Dirección : ');
+             echo '</td>';
+
+             echo '<td width=15% valing="top">';
+		 	$data = array(
+		 		'name'  => 'direccion',
+		 		'id'    => 'direccion',
+		 		'value' => set_value('direccion'),
+		 		'style' => 'width:100%'
+		 	);
+
+		 	echo form_input($data);
+		 	echo form_error('direccion');
+		 	echo '</td>';
+		 	echo'</tr>';
+
+echo'</table>';		 	
+echo'</div>';
+
+
+echo'<div id="tabs-3">';
+echo'<table>';
+
+            echo'<tr>';
+           echo '<td width=15% valing="top">';
             echo form_label('Fecha de Contacto : ');
+            echo '</td>';
+
+            echo '<td colpan="15%">';
 		 	$data = array(
 		 		'name'  => 'fecha_c_show',
 		 		'id'    => 'fecha_c_show',
 		 		'value' => set_value('fecha_c_show'),
-		 		'style' => 'width:18%'
+		 		'style' => 'width:10%'
 		 	);
 
 		 	echo form_input($data);
@@ -264,13 +303,16 @@
 		 	echo'</tr>';
 
             echo'<tr>';
-             echo '<td WIDTH=35 colspan="30%">';
+             echo '<td width=15% valing="top">';
 		 	 echo form_label('<br><br>Fecha de Visita : ');
+		 	 echo '</td>';
+
+		 	 echo '<td colpan="15%">';
 		 	$data = array(
 		 		'name'  => 'fecha_v_show',
 		 		'id'    => 'fecha_v_show',
 		 		'value' => set_value('fecha_v_show'),
-		 		'style' => 'width:30%'
+		 		'style' => 'width:15%'
 		 	);
 
 		 	echo form_input($data);
@@ -280,8 +322,11 @@
 
 
 		 	echo'<tr>';
-            echo '<td WIDTH=35 colspan="30%">';
+            echo '<td width=15%>';
             echo form_label('<br><br>Proyecto en Proceso :  ');
+            echo '</td>';
+            echo'<br>';
+            echo '<td colpan="15%">';
 		 	$data = array(
 		 		'name'  => 'status',
 		 		'id'    => 'status',
@@ -294,19 +339,19 @@
 		 	echo'</tr>';
 
             echo'<tr>';
-            echo '<td WIDTH=35 colspan="30%">';
+            echo '<td width=15% valing="top">';
              echo form_label(' <br><br>  PRODUCTOS:  <br><br>');
              echo'</tr>';
          
              foreach($aProductos as $aItem){
                 
                 echo'<tr>';
-            echo '<td WIDTH=35 colspan="30%">';
+                echo '<td width=15% valing="top">';
             	echo form_label($aItem->nombre);
             	echo'</tr>';
 
             	echo'<tr>';
-            echo '<td WIDTH=35 colspan="30%">';
+            echo '<td width=15% valing="top">';
             	$data = array(
                 	'name'        => 'productos[]',
                 	'value'       => $aItem->id, 
@@ -318,19 +363,9 @@
                  echo'</tr>';
 
              }
-              echo'<br>';
-              echo'<tr>';
-              echo '<td aling="rigth" width="100%" colspan="100%">';
-                $data = array(
-		 		'name'  => 'login',
-		 		'id'    => 'login',
-		 		'value' => 'Registro',
-		 		'style' => 'width:20%'
-		 	);
-
-		 	echo form_submit($data);
-		 	echo '</td>';
-		 	echo'</tr>';
+             
+         
+		 	
 
  echo '<input name="fecha_v" type="hidden" id="fecha_v" />';
  echo '<input name="fecha_c" type="hidden" id="fecha_c" />';
@@ -357,3 +392,24 @@ $('#fecha_c_show').datepicker({
 
 </script>
 </table>
+</div>
+</div>
+</body>
+</html>
+<?php
+
+ echo' <tr>';
+ echo '<center>'; 
+  echo'<td valing="center" colspan="100%">';
+                $data = array(
+		 		'name'  => 'login',
+		 		'id'    => 'login',
+		 		'value' => 'Registro',
+		 		'style' => 'width:20%'
+		 	);
+
+		 	echo form_submit($data);
+		 	echo'</form>';
+		 	echo'</center>';
+		 	
+		 	?>
